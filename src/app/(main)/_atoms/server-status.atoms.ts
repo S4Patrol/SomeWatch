ed5d846f2ddc027fece07ca48cd8bf47cfe7a672
PatrolsIsComeback <159645122+PatrolsIsComeback@users.noreply.github.com>
2026-05-01 08:31:00 +0300

    Add files via upload

---
+++ src/app/(main)/_atoms/server-status.atoms.ts
@@ -0,0 +1,11 @@
+import { Status } from "@/api/generated/types"
+import { atom } from "jotai"
+import { atomWithStorage } from "jotai/utils"
+
+export const serverStatusAtom = atom<Status | undefined>(undefined)
+
+export const isLoginModalOpenAtom = atom(false)
+export const isEmailLoginModalOpenAtom = atom(false)
+export const isAccountSettingsModalOpenAtom = atom(false)
+
+export const serverAuthTokenAtom = atomWithStorage<string | undefined>("sea-server-auth-token", undefined, undefined, { getOnInit: true })
